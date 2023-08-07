b628b7a0d16eb6e1278679c25ead8f3fb44ade55
João Carlos <doidobr46@gmail.com>
2023-08-07 19:07:19 -0300

    SplashPage Design implemented.

--- lib/app/modules/splashPage/splashPage.dart
+++ lib/app/modules/splashPage/splashPage.dart
@@ -3,13 +3,44 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SplashPage extends GetView<SplashPageController> {
+  const SplashPage({super.key});
+
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-    body: SafeArea(
-      child: Text('MyController'),)
+    return 
+    Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFFEAD6EE), Color(0xFFA0F1EA)], // You can customize the gradient colors here
+            ),
+          ),
+          child: const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Controle Acesso', // Replace 'MyApp' with your app's name or any other desired text
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 200),
+                CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
